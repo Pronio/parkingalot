841979716988b2708a0a57e31d97a1e534e4adb5
Pedro Direita <pedro.direita@gmail.com>
2019-10-14 01:10:31 +0100

    Add authentication template file

---
+++ parkingalot_server/authentication.php
@@ -0,0 +1,11 @@
+<?php
+	$host = "database_server";
+	$user = "user";
+	$pass = "*******";
+	$dsn = "mysql:host=$host;dbname=$user";
+	$acesstoken = "arbitrary_long_string_here";
+	
+	//error_reporting(E_ALL);
+	//ini_set('display_errors', 1);
+?>
+	
